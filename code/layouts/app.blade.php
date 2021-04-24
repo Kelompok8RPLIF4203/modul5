5ce13046cb7a284bca3e33225d645f599e1b65e3
mnaufalmuafa <naufalmuafa77@gmail.com>
2021-04-24 09:57:24 +0700

    Initial commit

---
+++ code/layouts/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  @include('includes.meta')
+  @stack('add-on-meta')
+  @include('includes.style')
+  @stack('add-on-style')
+  <title>@yield('title')</title>
+</head>
+<body>
+  @include('includes.navbar')
+  @yield('content')
+  @include('includes.script')
+  @stack('add-on-script')
+</body>
+</html>
